a218acb6c6011b407ff8699d7e2dfa9d7bef00f2
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-11-27 18:21:52 -0500

    Update group.js

--- tips/group.js
+++ tips/group.js
@@ -3,5 +3,11 @@ let group = client.getGroupById("6335c42a3402fdba72620e37")
 group.id//returns the id of the group
 group.name//returns the name of the group
 group.owner//returns the id of the groups owner
-
 group.users()//returns the users of the group in an array
+
+group.leave()//leave the given group
+group.invite("userid")//invite someone to the group
+group.edit({
+  name: 'new name of the group',//optional
+  invite: 'invite type of the group | either "members" or "owner"'//optional
+})
