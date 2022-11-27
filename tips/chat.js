50dbd5fbbf1de89406845cc0ba51483e4f521879
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-11-27 17:13:06 -0500

    Update chat.js

--- tips/chat.js
+++ tips/chat.js
@@ -1,13 +1,12 @@
-client.onPost(post => {
-  post.onChat(chat => {
-    chat.id//returns the chats id
-    chat.author//returns the author of the chat | check user.js for the properties extending this property
-    chat.text//returns the text of the chat
-    chat.reply("text")//replies to the chat
-    
-    chat.delete()//deletes chat sent by you
-    chat.edit("text")//edits chat sent by you
-    
-    chat.report("reason", "report")//reports the chat
-  })
-})
+let chat = await client.getChatById("6383dfd8f06958b583a92436")
+
+//general
+chat.id//returns the chats id
+chat.author//returns the author of the chat | check user.js for the properties extending this property
+chat.text//returns the text of the chat
+chat.reply("text")//replies to the chat
+chat.report("reason", "report")//reports the chat
+
+//self chat
+chat.delete()//deletes chat
+chat.edit("text")//edits chat
