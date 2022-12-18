ef851b3a103a66fadcd9c7e430207f31fa0343c5
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-12-18 13:41:47 -0500

    Update chat.js

--- tips/chat.js
+++ tips/chat.js
@@ -1,13 +1,13 @@
-let chat = await client.getChatById("6383dfd8f06958b583a92436")
+client.getChatById("6383dfd8f06958b583a92436").then(chat => {
+  //general
+  chat.id//returns the chats id
+  chat.author//returns the author of the chat | check user.js for the properties extending this property
+  chat.text//returns the text of the chat
+  chat.reply("text")//replies to the chat
+  chat.report("reason", "report")//reports the chat
 
-//general
-chat.id//returns the chats id
-chat.author//returns the author of the chat | check user.js for the properties extending this property
-chat.text//returns the text of the chat
-chat.reply("text")//replies to the chat
-chat.report("reason", "report")//reports the chat
-
-//self chat
-chat.delete()//deletes chat
-//premium
-chat.edit("text")//edits chat
+  //self chat
+  chat.delete()//deletes chat
+  //premium
+  chat.edit("text")//edits chat
+})
