c500bb3a54d2cde8ecf6282ba20b2e8859a0000c
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-12-18 13:43:57 -0500

    Update user.js

--- tips/user.js
+++ tips/user.js
@@ -1,27 +1,27 @@
-let user = await client.getUserById("6154f0d0a8d6d106c5b869b6") // you need an async function for this to work
+client.getUserById("6154f0d0a8d6d106c5b869b6").then(user => {
+  user.classStatus//returns false if user privacy is not public, otherwise, itll return true
 
-user.classStatus//returns false if user privacy is not public, otherwise, itll return true
+  user.id//userid of the user
+  user.username//username of the user
+  user.roles//more than one role: array | one role: string
 
-user.id//userid of the user
-user.username//username of the user
-user.roles//more than one role: array | one role: string
+  user.settings.profilePicture//profile picture id of the user
+  user.settings.profileBanner//profile banner id of the user
+  user.settings.description//bio of the user
+  user.settings.visibility//visibility of the user
 
-user.settings.profilePicture//profile picture id of the user
-user.settings.profileBanner//profile banner id of the user
-user.settings.description//bio of the user
-user.settings.visibility//visibility of the user
+  user.status().raw//raw number status
+  user.status().parsed//returns: status of user, "online", "offline", "group"
 
-user.status().raw//raw number status
-user.status().parsed//returns: status of user, "online", "offline", "group"
+  user.follows.following//number of people user is following
+  user.follows.followers//number of people following the user
 
-user.follows.following//number of people user is following
-user.follows.followers//number of people following the user
+  user.follow()//follows the given user
+  user.unfollow()//unfollows the given user
 
-user.follow()//follows the given user
-user.unfollow()//unfollows the given user
+  user.report("reason", "report")//reports a user
 
-user.report("reason", "report")//reports a user
-
-//mod
-user.ban("length", "reason", "terminate")//ban the user selected | length: required, reason: required, terminate: optional
-user.unban()//unbans a user
+  //mod
+  user.ban("length", "reason", "terminate")//ban the user selected | length: required, reason: required, terminate: optional
+  user.unban()//unbans a user
+})
