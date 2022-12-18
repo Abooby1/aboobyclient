32467dae76c3ec6c6ac24dd8dbe9cbebeb08db6d
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-12-18 13:42:17 -0500

    Update group.js

--- tips/group.js
+++ tips/group.js
@@ -1,13 +1,13 @@
-let group = client.getGroupById("6335c42a3402fdba72620e37")
+client.getGroupById("6335c42a3402fdba72620e37").then(group => {
+  group.id//returns the id of the group
+  group.name//returns the name of the group
+  group.owner//returns the id of the groups owner
+  group.users()//returns the users of the group in an array
 
-group.id//returns the id of the group
-group.name//returns the name of the group
-group.owner//returns the id of the groups owner
-group.users()//returns the users of the group in an array
-
-group.leave()//leave the given group
-group.invite("userid")//invite someone to the group
-group.edit({
-  name: 'new name of the group',//optional
-  invite: 'invite type of the group | either "members" or "owner"'//optional
+  group.leave()//leave the given group
+  group.invite("userid")//invite someone to the group
+  group.edit({
+    name: 'new name of the group',//optional
+    invite: 'invite type of the group | either "members" or "owner"'//optional
+  })
 })
