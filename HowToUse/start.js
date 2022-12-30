8eb7a3618c2fe9edad8e1c325cfe5c3df891720d
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-12-29 21:19:11 -0500

    Create start.js

---
+++ HowToUse/start.js
@@ -0,0 +1,10 @@
+import {Client} from 'aboobyclient'
+
+const client = new Client({
+  userid: 'bot userid',
+  token: 'bot token (gotten from https://docs.google.com/forms/d/e/1FAIpQLSfphZMvTNPCI0yicT9js0Nty8EFybjoOiKCKkk2FtZ1dTOQXQ/viewform)',
+  config: {//object "config" is optional
+    'statcontrib': false,//default: false | All bot actions contribute to the bot https://app.photop.live/?from=launchpage&user=621cba7663790d5ac3c2aca6#profile | boolean
+    'groupConnection': true//default: true | true: group posts are connected with the default onPost | false: group posts arent connected with the default onPost, youll have to do onPost(callback => {}, groupid)
+  }
+})
