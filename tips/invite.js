d1ddc43c416d7e5484d540b6f78845c02ac5e3a1
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2022-12-29 21:19:53 -0500

    Delete tips directory

--- tips/invite.js
+++
@@ -1,4 +0,0 @@
-client.onInvite(invite => {
-  invite.accept()//accepts the invite
-  invite.deny()//denies the invite
-})
--- tips/onLike.js
+++
@@ -1,6 +0,0 @@
-client.onPost(post => {
-  post.onLike(like => {
-    like.change//returns a number, either 1 or -1 based on if its a like or a dislike
-    like.user//returns the user who liked | check user.js for more properties extending this property
-  })
-})
