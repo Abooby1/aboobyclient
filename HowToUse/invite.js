7175084ee2b8812b2cf14e8ce687e967b29a3560
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2023-05-15 10:42:21 -0400

    Update invite.js

--- HowToUse/invite.js
+++ HowToUse/invite.js
@@ -1,4 +1,10 @@
-client.onInvite(invite => {
+client.on("invite", (invite) => {
   invite.accept()//accepts the invite
   invite.deny()//denies the invite
+})
+
+group.getInvites(invites => {
+  invites.forEach(invite => {
+    invite.revoke()
+  })
 })
