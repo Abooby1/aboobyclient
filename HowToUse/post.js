f511029dfdc850a2033519b8c98cba13e661f2aa
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2023-05-15 10:26:16 -0400

    Update post.js

--- HowToUse/post.js
+++ HowToUse/post.js
@@ -1,32 +1,27 @@
 client.getPostById("638253dad62f9d79c6459c86").then(post => {
   //utilities
   post.cache()//adds post data to client.postCache
+  post.disconnect()//disconnects all listeners of the post | IMPORTANT: if not used bot might get rate limited
   
   //general
   post.id//returns id of the post
   post.author//returns the author of the post | check user.js for the properties extending this property
   post.text//returns the text of the post
+  post.media//returns the media of the post
 
-  post.stats.likes//returns the likes of the post
-  post.stats.quotes//returns the quotes of the post
-  post.stats.chats//returns the number of chats in the post
+  post.stats.likes//returns the number of likes on the post
+  post.stats.quotes//returns the number of quotes on the post
+  post.stats.chats//returns the number of chats on the post
+  
+  post.like()//likes the post
+  post.dislike()//unlikes the post if liked
 
   post.report("reason", "report")//reports the post
 
-  post.chat("text")//chats in a post without reply
+  post.chat("text", "replyid (optional)")//chats in a post without reply
   
   post.on(type, function() {
     //main listener for post | check on.js for properties
-  })
-
-  post.onChat(callback => {
-    //check onChat.js for information
-  })
-  post.onLike(callback => {
-    //check onLike.js for information
-  })
-  post.onEdit(callback => {
-    //check onEdit.js for information
   })
 
   //self
@@ -34,5 +29,5 @@ client.getPostById("638253dad62f9d79c6459c86").then(post => {
   post.unpin()//unpins the post if the post is yours
   post.delete()//deletes the post if its yours
   //premium
-  post.edit("text", ["images"])//edits the post if bot has premium
+  post.edit("text")//edits the post if bot has premium
 })
