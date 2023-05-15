208473931ee09c6496655ccbfa4bc1e7069c3260
Abooby1 <97899737+Abooby1@users.noreply.github.com>
2023-05-15 10:37:21 -0400

    Update chat.js

--- HowToUse/chat.js
+++ HowToUse/chat.js
@@ -3,15 +3,18 @@ client.getChatById("6383dfd8f06958b583a92436").then(chat => {
   chat.id//returns the chats id
   chat.author//returns the author of the chat | check user.js for the properties extending this property
   chat.text//returns the text of the chat
+  chat.postid//returns the postid of the chat
+  chat.replyid//returns the replyid of the chat if it has one
+  
   chat.reply("text")//replies to the chat
-  chat.report("reason", "report")//reports the chat
+  
+  chat.report({//reports the chat
+    reason: "",
+    report: ""
+  })
   
   chat.on(type, function() {
     //main listener for chat events | check on.js for properties
-  })
-  
-  chat.onEdit(callback => {
-    //check onEdit.js for more information
   })
 
   //self chat
